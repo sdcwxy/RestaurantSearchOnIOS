b15f8c38d9fc1b634d4ecaa18cfe4a49241c9baf
sdcwxy <zhonglinsun517@gmail.com>
2017-10-22 19:24:53 -0700

    Initial Commit

---
+++ RestaurantSearchOnIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestaurantSearchOnIOS
+//
+//  Created by zhonglis on 10/22/17.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RestaurantSearchOnIOSTests/RestaurantSearchOnIOSTests.m
@@ -0,0 +1,39 @@
+//
+//  RestaurantSearchOnIOSTests.m
+//  RestaurantSearchOnIOSTests
+//
+//  Created by zhonglis on 10/22/17.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RestaurantSearchOnIOSTests : XCTestCase
+
+@end
+
+@implementation RestaurantSearchOnIOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
